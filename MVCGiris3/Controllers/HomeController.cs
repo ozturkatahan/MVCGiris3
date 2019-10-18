ee4af1e787abeca1050ef9281350543b08cde2c7
atahan.ozturk <ozturkatahan5@gmail.com>
2019-10-18 21:30:55 +0300

    Ana Sayfa Editing

--- MVCGiris3/Controllers/HomeController.cs
+++ MVCGiris3/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCGiris3.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,11 @@ namespace MVCGiris3.Controllers
 {
     public class HomeController : Controller
     {
+        KisiContext db = new KisiContext();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(db.Kisiler.ToList());
         }
     }
 }
